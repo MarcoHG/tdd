091af11fc8fd60589f7898c3a4e5dfd0280cf2fd
MarcoHG <marco.henrygin@gmail.com>
2020-03-11 13:01:11 -0700

    improve portability of build.sh for SprintfTest

--- jg/SprintfTest/build.sh
+++ jg/SprintfTest/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-PROJECT=/home/marco/Documents/edu/tdd/gitTDD/jg
-FIXTURE_SRC=$PROJECT/code/unity.framework/extras/fixture/src
-UNITY_SRC=$PROJECT/code/unity.framework/src
-gcc AllTests.c SprintfTest.c SprintfTestRunner.c -I$FIXTURE_SRC -I$UNITY_SRC $FIXTURE_SRC/unity_fixture.c $UNITY_SRC/unity.c -o testRun
+# source this file -or- run as script 
+PROJECT=$(pwd)
+FIXTURE_SRC=$PROJECT/../code/unity.framework/extras/fixture/src
+UNITY_SRC=$PROJECT/../code/unity.framework/src
+FILES="AllTests.c SprintfTest.c SprintfTestRunner.c  $FIXTURE_SRC/unity_fixture.c $UNITY_SRC/unity.c"
+gcc -I$FIXTURE_SRC -I$UNITY_SRC $FILES -o testRun
 
